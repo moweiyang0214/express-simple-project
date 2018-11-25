bdc20ba1a9d5b780c3930fa55d16eca893ad02a7
moweiyang0214 <moweiyang19920214@gmail.com>
2018-11-25 22:17:20 +0900

    refactor for the code

--- index.js
+++ index.js
@@ -19,12 +19,15 @@ app.get('/api/courses', (req,res) => {
   res.send(courses)
 })
 
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(e => e.id === parseInt(req.params.id))
+  if(!course) return res.status(404).send('the course with the given Id was not found')
+  res.send(course)
+})
+
 app.post('/api/courses', (req, res) => {
   const { error } = validateCourse(req.body)
-  if(error){
-    res.status(400).send(error.details[0].message)
-    return
-  }
+  if(error) return res.status(400).send(error.details[0].message)
 
   const course = {
     id: courses.length + 1,
@@ -34,39 +37,15 @@ app.post('/api/courses', (req, res) => {
   res.send(course)
 })
 
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(e => e.id === parseInt(req.params.id))
-  if(!course) { //404
-    res.status(404).send('the course with the given Id was not found')
-  } else {
-    res.send(course)
-  }
-})
-
-app.get('/api/posts/:year/:month', (req, res)=> {
-  res.send(req.query)
-  // res.send(req.params)
-})
-// PORT: use command `export PORT=5000` to set your process variable
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}...`)
-})
-
 app.put('/api/courses/:id', (req, res)=> {
   // Look up the course
   // if not existing, return 404
   const course = courses.find(e => e.id === parseInt(req.params.id))
-  if(!course) { //404
-    res.status(404).send('the course with the given Id was not found')
-  }
+  if(!course) return res.status(404).send('the course with the given Id was not found')
   // validate
   // if invalid, return 400 - Bad request
   const { error } = validateCourse(req.body) // result.error
-  if(error){
-    res.status(400).send(error.details[0].message)
-    return
-  }
+  if(error) return res.status(400).send(error.details[0].message)
   // update course
   course.name = req.body.name
   // return the updated course
@@ -78,9 +57,7 @@ app.delete('/api/courses/:id', (req, res)=> {
   // Lookup the course
   // not existing , return 404
   const course = courses.find(e => e.id === parseInt(req.params.id))
-  if(!course) { //404
-    res.status(404).send('the course with the given Id was not found')
-  }
+  if(!course) return res.status(404).send('the course with the given Id was not found')
   // delete
   const index = courses.indexOf(course)
   courses.splice(index, 1)
@@ -95,3 +72,14 @@ function validateCourse(course) {
 
   return Joi.validate(course, schema)
 }
+
+// app.get('/api/posts/:year/:month', (req, res)=> {
+//   res.send(req.query)
+//   // res.send(req.params)
+// })
+
+// PORT: use command `export PORT=5000` to set your process variable
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`)
+})
