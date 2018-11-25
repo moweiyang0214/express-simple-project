039354ef3522c9644e22c929531c2257a073fe33
moweiyang0214 <moweiyang19920214@gmail.com>
2018-11-25 21:29:29 +0900

    add simple logic for req.params and req.query

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const app = express();
 
+const courses = [
+  {id: 1, name: 'course1'},
+  {id: 2, name: 'course2'},
+  {id: 3, name: 'course3'}
+]
+
 app.get('/', (req, res) => {
   res.send('Hello world!!!')
 })
 
 app.get('/api/courses', (req,res) => {
-  res.send([1,2,3])
+  res.send(courses)
 })
 
+app.get('/api/courses/:course_id', (req, res) => {
+  const course = courses.find(e => e.id === parseInt(req.params.course_id))
+  if(!course) { //404
+    res.status(404).send('the course with the given Id was not found')
+  } else {
+    res.send(course)
+  }
+})
+
+app.get('/api/posts/:year/:month', (req, res)=> {
+  res.send(req.query)
+  // res.send(req.params)
+})
 // PORT: use command `export PORT=5000` to set your process variable
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`listening on port ${port}...`)
 })
 // app.post()
