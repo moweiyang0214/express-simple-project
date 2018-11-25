fa42e5a2c6937504e2d117a95dfcb9dcd34b668a
moweiyang0214 <moweiyang19920214@gmail.com>
2018-11-25 21:18:13 +0900

    set PORT environment variable

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req,res) => {
   res.send([1,2,3])
 })
-app.listen(3000, ()=>{
-  console.log('listening on port 3000...')
+
+// PORT: use command `export PORT=5000` to set your process variable
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+  console.log(`listening on port ${port}...`)
 })
 // app.post()
 // app.put()
